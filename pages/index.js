be75ed6c7fde74c2736ecaf71bf036386edae7fc
Alejandro <tk.jalejandro@gmail.com>
2021-10-16 17:06:32 +0200

    understanding of getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,57 @@
-import Layout from "../components/layout/Layout";
 import MeetUpList from "../components/meetups/MeetupList"
 
-const HomePage = () => {
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "some Title",
+    image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
+    address: "Cartagena old City",
+    description: "Here goes some description"
+  },
+  {
+    id: "m2",
+    title: "meet up 2",
+    image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
+    address: "Cartagena old City",
+    description: "Here goes some description"
+  },
+  {
+    id: "m3",
+    title: "meet up 3",
+    image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
+    address: "Cartagena old City",
+    description: "Here goes some description"
+  }
 
-  const DUMMY_MEETUPS = [
-    {
-      id: "m1",
-      title: "some Title",
-      image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
-      address: "Cartagena old City",
-      description: "Here goes some description"
-    },
-    {
-      id: "m2",
-      title: "meet up 2",
-      image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
-      address: "Cartagena old City",
-      description: "Here goes some description"
-    },
-    {
-      id: "m3",
-      title: "meet up 3",
-      image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
-      address: "Cartagena old City",
-      description: "Here goes some description"
-    }
+]
+const HomePage = (props) => {
 
-  ]
+
   return (
     <>
 
       {/* This MeetUpList Component is taking an array as a prop and map it inside. */}
-      <MeetUpList meetups={DUMMY_MEETUPS} />
+      <MeetUpList meetups={props.meetups} />
 
 
     </>
   );
 };
+//This is executed in build process not in server and not in client.
 
+// export async function getStaticProps() {
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   }
+// }
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    }
+  }
+}
 export default HomePage;
