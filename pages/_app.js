c62367379129660c85ad52226b96cf3285459c6c
Alejandro <tk.jalejandro@gmail.com>
2021-10-16 15:36:05 +0200

    adding File & wrapper components. Now we have layout covering all the props for main.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Layout from '../components/layout/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  {/* Here we use layout to put of navigation and other stuff, all the childrens will be inside. This page is the children */ }
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,39 @@
+import Layout from "../components/layout/Layout";
 import MeetUpList from "../components/meetups/MeetupList"
 
 const HomePage = () => {
 
-const DUMMY_MEETUPS = [
+  const DUMMY_MEETUPS = [
     {
-        id: "m1",
-        title: "some Title",
-        image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
-        address: "Cartagena old City",
-        description: "Here goes some description"
+      id: "m1",
+      title: "some Title",
+      image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
+      address: "Cartagena old City",
+      description: "Here goes some description"
     },
     {
-        id: "m2",
-        title: "meet up 2",
-        image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
-        address: "Cartagena old City",
-        description: "Here goes some description"
+      id: "m2",
+      title: "meet up 2",
+      image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
+      address: "Cartagena old City",
+      description: "Here goes some description"
     },
     {
-        id: "m3",
-        title: "meet up 3",
-        image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
-        address: "Cartagena old City",
-        description: "Here goes some description"
+      id: "m3",
+      title: "meet up 3",
+      image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
+      address: "Cartagena old City",
+      description: "Here goes some description"
     }
 
-]
+  ]
   return (
     <>
-    {/* This MeetUpList Component is taking an array as a prop and map it inside. */}
-      <MeetUpList meetups={DUMMY_MEETUPS}/>
-      
+
+      {/* This MeetUpList Component is taking an array as a prop and map it inside. */}
+      <MeetUpList meetups={DUMMY_MEETUPS} />
+
+
     </>
   );
 };
