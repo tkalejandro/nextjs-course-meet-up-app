6decedcbdcfb026d72495403e0ee9a196c0f21d9
Alejandro <tk.jalejandro@gmail.com>
2021-10-22 15:26:08 +0200

    Instal mongoDB in the project and succesfully conect the mongoDB to the project.

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,24 @@
-
+import { MongoClient } from "mongodb"
 
 //ONLY POST  is triggered here
-const handler (req, res) {
+const handler = async (req, res) => {
 
     if (req.method === "POST") {
         const data = req.body
         const {title, image, address, description} = data
+
+
+        const client = await MongoClient.connect("mongodb+srv://admin:adminadmin@cluster0.55hxa.mongodb.net/meetUps?retryWrites=true&w=majority")
+
+        const db = client.db()
+
+        //table
+        const meetupsCollection = db.collection("meetups")
+        const result = await meetupsCollection.insertOne({data})
+        console.log(result)
+
+        client.close()
+        res.status(201).json({message: "MeetUp Inserted"})
     }
 
 }
