8a5765f171998765d05da8ec02f6818c95242c8a
Alejandro <tk.jalejandro@gmail.com>
2021-10-22 11:17:30 +0200

    learned about the getStaticPath and how it should be build together with getStaticProps in a dynamic page.

--- pages/[meetUpId]/index.js
+++ pages/[meetUpId]/index.js
@@ -1,18 +1,61 @@
 
+//import { getStaticProps } from ".."
 import MeetUpDetail from "../../components/meetups/MeetUpDetail"
 
 
 const MeetUpDetails = () => {
     return (
-    
-        <MeetUpDetail 
+
+        <MeetUpDetail
             image="https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg"
             title="First Meeting"
             address="Some street somewhere"
             description="Here goes some description"
         />
-    
+
     )
+}
+export const getStaticPaths = () => {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetUpId: "m1"
+                }
+            },
+            {
+                params: {
+                    meetUpId: "m2"
+                }
+            },
+            {
+                params: {
+                    meetUpId: "m3"
+                }
+            }
+        ]
+    }
+}
+
+
+export const getStaticProps = async (context) => {
+
+    const meetUpId = context.params.meetUpId
+    console.log(meetUpId)
+    //Fetch Data
+
+    return {
+        props: {
+            meetUpData: {
+                id: meetUpId,
+                image: "https://image.geo.de/30143558/t/Xj/v4/w1440/r0/-/-cartagena-m-eg695b-jpg--81730-.jpg",
+                title: "First Meeting",
+                address: "Some street somewhere",
+                description: "Here goes some description"
+            }
+        }
+    }
 }
 
 export default MeetUpDetails
