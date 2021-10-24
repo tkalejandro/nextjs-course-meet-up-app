00cd30385acb08b91c63beb64d90276fd45694ba
Alejandro <tk.jalejandro@gmail.com>
2021-10-24 11:29:55 +0200

    Learned how to do my first use of MongoDB and how to fetch them in the api folder.

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,25 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm"
-
+import {useRouter} from "next/router"
 
 const NewMeetUpPage = () => {
+    const router = useRouter()
 
-    const addMeetUpHandler = (enteredMeetUpData) => {
-        console.log(enteredMeetUpData)
+    const addMeetUpHandler = async (enteredMeetUpData) => {
+        //Because we are building or server side here in the same folder, we just need to write the url inside the api folder location, then the code it will be triggered.
+
+        const response = await fetch("/api/new-meetup", {
+            method: "POST",
+            body: JSON.stringify(enteredMeetUpData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await response.json()
+
+        console.log(data)
+        //We install router next js to so after all this happen we transfer the use to the homepage. We can use replace() so user cant go back, or push() user can go back.
+
+        router.push("/")
     }
 
     return (
